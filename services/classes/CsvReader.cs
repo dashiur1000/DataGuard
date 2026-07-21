a90d9ab3116e09cfbdbe353fb5f0be1f6372f5fa
Mendel <MendelCohen770@gmail.com>
2026-07-21 11:33:13 +0300

    feat: add CsvReader

--- services/classes/CsvReader.cs
+++ services/classes/CsvReader.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
 using System.Text;
 
 namespace DataGuard.services.classes
 {
-    internal class CsvReader
+    class CsvReader : IReader 
     {
+        static string[] ReadFile(string path)
+        {
+            string[] fileText = File.ReadAllLines(path);
+
+            return fileText;
+        }
     }
 }
