bfdb6b2ea42e70aec653059e47230ef1adeb7f99
Mendel <MendelCohen770@gmail.com>
2026-07-21 12:18:37 +0300

    feat: create module - get the text from the file

--- services/classes/ModuleCreator.cs
+++ services/classes/ModuleCreator.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace DataGuard.services.classes
 {
+    /// <summary>
+    /// the orchestrator that in charge of the module creating
+    /// </summary>
     internal class ModuleCreator
     {
+
+        void Create()
+        {
+            string filePath = "./input/Buys_Computer_Test.csv"; //TODO: Change the path
+            CsvReader csvReader = new CsvReader();
+
+            string[] fileText = csvReader.ReadFile(filePath);
+
+
+
+
+        }
+
+
     }
 }
