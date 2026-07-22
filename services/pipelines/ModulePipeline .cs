6ae7c7a7a78a7ff9fcb7d26e6c5983a7436cfa2f
david stern <dzs101010@gmail.com>
2026-07-22 07:54:28 +0300

    first pipeline

--- services/pipelines/ModulePipeline .cs
+++ services/pipelines/ModulePipeline .cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using DataGuard.services.classes;
+using DataGuard.services.interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataGuard.services.pipelines
 {
-    internal class ModulePipeline
+    class ModulePipeline
     {
+        static public ModuleCreator FirstPipeline(string[] args)
+        {
+            FindPathInFile pathFinder = new FindPathInFile();
+            IReader reader = new CsvReader();
+            IParser parser = new DataParsing();
+            IModule module = new ModuleCreator();
+
+            string TrainingPath = pathFinder.FoundPath(args[0]);
+            string[] lines = reader.ReadFile(TrainingPath);
+            List<Dictionary<string, string>> Model = parser.DictionaryParser(lines);
+            return Model;
+        }
     }
 }
