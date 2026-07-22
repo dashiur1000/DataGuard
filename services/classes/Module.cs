32b9c3e0d2b371f9eb744dc1691f74861245674b
Mendel <MendelCohen770@gmail.com>
2026-07-22 09:39:04 +0300

    feat: module calculate - adding cond

--- services/classes/Module.cs
+++ services/classes/Module.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace DataGuard.services.classes
@@ -15,16 +16,65 @@ namespace DataGuard.services.classes
                 .Distinct()
                 .ToList();
 
+            Dictionary<string, int> priors = GetPriors(labels, inputData, targetColumn);
+
+            Dictionary<(string, string, string), double> cond = new();
+            Dictionary<(string, string, string), double> unseen = new();
+
+            foreach (string label in labels)
+            {
+                var Rows = inputData.Where(dict => dict[targetColumn] == label);
+
+
+                List<string> features = Rows.ToList()[0].Keys.ToList(); // gets the columns names
+                foreach (var feature in features)
+                {
+                    if (feature == targetColumn) { continue; } // skips the target column
+                    int distinct = inputData.Select(row => row[feature]).Distinct().Count();
+
+                    List<string> values = inputData.Select(row => row[feature]).Distinct().ToList();
+                    
+                    foreach (string value in values)
+                    {
+                        Console.WriteLine(value);
+                        double match = Rows.Count(row => row[feature] == value);
+                        
+                        cond[(label, feature, value)] = (double)(match + 1) / (Rows.Count() + distinct);
+                    }
+                }
+            }
+
+            foreach (var c in cond)
+            {
+                Console.WriteLine($"k: {c.Key} : v: {c.Value}");
+            }
+
+            return new Dictionary<(string, string, string), double>();
+        }
+
+
+        /// <summary>
+        /// gets the count of each label in the target column.
+        /// </summary>
+        /// <param name="labels"></param>
+        /// <param name="inputData"></param>
+        /// <param name="targetColumn"></param>
+        /// <returns></returns>
+        Dictionary<string, int> GetPriors(List<string> labels, List<Dictionary<string, string>> inputData, string targetColumn)
+        {
             Dictionary<string, int> priors = new();
 
             foreach (string label in labels)
             {
-                priors[label] = inputData.Count(row => row[targetColumn] == label); 
+                priors[label] = inputData.Count(row => row[targetColumn] == label);
             }
 
+            return priors;
+        }
 
+        void GetCond()
+        {
 
-            return new Dictionary<(string, string, string), double>();
-        }   
+        }
     }
 }
