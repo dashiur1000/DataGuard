f966df68cbc0f6c5b1c7daa916963e01756295a2
david stern <dzs101010@gmail.com>
2026-07-22 07:59:40 +0300

    fix findPath and IPrediction

--- services/classes/FindPath.cs
+++ services/classes/FindPath.cs
@@ -9,7 +9,7 @@ namespace DataGuard.services.classes
         public string FoundPath(string filename, params string[] dir)
         {
             string baseDir = AppDomain.CurrentDomain.BaseDirectory;
-            DirectoryInfo? projectDir = Directory.GetParent(baseDir).Parent.Parent.Parent;
+            DirectoryInfo projectDir = Directory.GetParent(baseDir).Parent.Parent.Parent;
             string FullPath = Path.Combine(projectDir.FullName, Path.Combine(dir), filename);
             return FullPath;
         }
--- services/interfaces/IPrediction .cs
+++ services/interfaces/IPrediction .cs
@@ -1,3 +1,4 @@
+using DataGuard.services.classes;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -5,5 +6,5 @@ using System.Text;
 namespace DataGuard.services.interfaces;
 interface IPrediction
 {
-    string Predict(NaiveBayesModel model, Dictionary<string, string> sample);
+    string Predict(ModuleCreator model, Dictionary<string, string> sample);
 }
